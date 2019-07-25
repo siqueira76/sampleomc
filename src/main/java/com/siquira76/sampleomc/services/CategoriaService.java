ccd96fa1c75d28789b596c7aece270131c7bb3d2
José Carlos Siqueira <josecarlos.siqueira76@gmail.com>
2019-07-25 11:07:39 -0300

    Deletando Categoria com DELETE

--- src/main/java/com/siquira76/sampleomc/services/CategoriaService.java
+++ src/main/java/com/siquira76/sampleomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.siquira76.sampleomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.siquira76.sampleomc.domain.Categoria;
 import com.siquira76.sampleomc.repositories.CategoriaRepository;
+import com.siquira76.sampleomc.services.exceptions.DataIntegrityExeption;
 import com.siquira76.sampleomc.services.exceptions.ObjectNotFondException;
 
 @Service
@@ -17,7 +19,7 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElseThrow(()-> new ObjectNotFondException("Objeto não emcontrado id: " 
+		return obj.orElseThrow(()-> new ObjectNotFondException("Objeto não encontrado id: " 
 		+ id + ", Tipo: " + Categoria.class.getName()));
 	}
 	
@@ -29,6 +31,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		buscar(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		buscar(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityExeption("Não é possivel excluir uma categoria que poossui Produto");
+		}
 	}
 	
 }
---
+++ src/main/java/com/siquira76/sampleomc/services/exceptions/DataIntegrityExeption.java
@@ -0,0 +1,14 @@
+package com.siquira76.sampleomc.services.exceptions;
+
+public class DataIntegrityExeption extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public DataIntegrityExeption(String msg) {
+		super(msg);
+	}
+	
+	public DataIntegrityExeption(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+}
