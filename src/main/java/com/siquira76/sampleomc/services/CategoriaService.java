f99e043acdbc3190f5ddc176a601ecbd13879017
José Carlos Siqueira <josecarlos.siqueira76@gmail.com>
2019-07-26 17:32:28 -0300

    Correcoes: update em CategoriaService e construtor Pagamento

--- src/main/java/com/siquira76/sampleomc/services/CategoriaService.java
+++ src/main/java/com/siquira76/sampleomc/services/CategoriaService.java
@@ -34,8 +34,9 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria obj) {
-		buscar(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = buscar(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 
 	public void delete(Integer id) {
@@ -58,6 +59,10 @@ public class CategoriaService {
 	
 	public Categoria fromDTO(CategoriaDTO objDTO) {
 		return new Categoria(objDTO.getId(), objDTO.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 	
 }
