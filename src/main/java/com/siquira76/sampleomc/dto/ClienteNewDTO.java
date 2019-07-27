85a1132486e910b356fbb0e4cc4b8af56af2cf3d
José Carlos Siqueira <josecarlos.siqueira76@gmail.com>
2019-07-27 18:20:00 -0300

    Validacao customizada de CPF e CNPJ na insercao de Cliente

--- src/main/java/com/siquira76/sampleomc/dto/ClienteNewDTO.java
+++ src/main/java/com/siquira76/sampleomc/dto/ClienteNewDTO.java
@@ -2,6 +2,9 @@ package com.siquira76.sampleomc.dto;
 
 import java.io.Serializable;
 
+import com.siquira76.sampleomc.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
