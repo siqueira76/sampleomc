cff1ddd405813fddf6548ca6ef755045f70916f3
José Carlos Siqueira <josecarlos.siqueira76@gmail.com>
2019-07-27 15:52:06 -0300

    Ajustando a delecao de Cliente e seu Endereco

--- src/main/java/com/siquira76/sampleomc/services/ClienteService.java
+++ src/main/java/com/siquira76/sampleomc/services/ClienteService.java
@@ -56,7 +56,7 @@ public class ClienteService {
 		try {
 			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityExeption("Não é possivel excluir pois possui entidades relacionadas");
+			throw new DataIntegrityExeption("Não é possivel excluir pois possui Pedidos relacionadas");
 		}
 	}
 
