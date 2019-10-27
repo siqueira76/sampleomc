02a273ebfe8d715dad4421cb0828e0aa139c4b55
José Carlos Siqueira <josecarlos.siqueira76@gmail.com>
2019-10-27 16:05:03 -0300

    Implementando autorizacao

--- src/main/java/com/siquira76/sampleomc/config/SecurityConfig.java
+++ src/main/java/com/siquira76/sampleomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.siquira76.sampleomc.security.JWTAuthenticationFilter;
+import com.siquira76.sampleomc.security.JWTAuthorizationFilter;
 import com.siquira76.sampleomc.security.JWTUtil;
 
 @Configuration
@@ -57,6 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(HttpMethod.GET, PUBLIC_MACHERS_GET).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
