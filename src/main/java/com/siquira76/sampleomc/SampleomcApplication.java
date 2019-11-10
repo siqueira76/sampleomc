0eeb2f41d97174fdd3951b0cdc4480bfc84bc946
José Carlos <josecarlos.siqueira76@gmail.com>
2019-11-10 19:19:12 -0200

    Salvando primeiro arquivo no S3

--- src/main/java/com/siquira76/sampleomc/SampleomcApplication.java
+++ src/main/java/com/siquira76/sampleomc/SampleomcApplication.java
@@ -1,11 +1,17 @@
 package com.siquira76.sampleomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.siquira76.sampleomc.services.S3Service;
+
 @SpringBootApplication
 public class SampleomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleomcApplication.class, args);
@@ -13,7 +19,7 @@ public class SampleomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		s3Service.uploadFile("C:\\Users\\T3575774\\Documents\\Geral\\images\\lampada.jpg");
 	}
 
 }
