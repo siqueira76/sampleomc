a453b81677347c9ccc2d052f92a31257e5d7cbb9
José Carlos Siqueira <josecarlos.siqueira76@gmail.com>
2020-01-11 15:14:20 -0300

    Bônus: ajustando tamanho da imagem - fixing missed files

--- src/main/java/com/siquira76/sampleomc/services/ClienteService.java
+++ src/main/java/com/siquira76/sampleomc/services/ClienteService.java
@@ -130,6 +130,10 @@ public class ClienteService {
 		}
 		
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
+		
 		String fileName = prefix + user.getId() + ".jpg";
 		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
 	}
