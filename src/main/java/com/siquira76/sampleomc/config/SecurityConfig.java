75419dad02c61121435f5340f59a73d626892c6d
José Carlos Siqueira <josecarlos.siqueira76@gmail.com>
2020-07-01 10:35:24 -0300

    Swagger - Parte 1

--- src/main/java/com/siquira76/sampleomc/config/SecurityConfig.java
+++ src/main/java/com/siquira76/sampleomc/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -27,54 +28,42 @@ import com.siquira76.sampleomc.security.JWTUtil;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
 	private UserDetailsService userDetailsService;
-	
+
 	@Autowired
 	private JWTUtil jwtUtil;
-	
+
 	@Autowired
-    private Environment env;
-	
-	private static final String[] PUBLIC_MACHERS = {
-			"/h2-console/**"
-	};
-	
-	private static final String[] PUBLIC_MACHERS_GET = {
-			"/produtos/**",
-			"/categorias/**",
-			"/estados/**"
-	};
-	
-	private static final String[] PUBLIC_MACHERS_POST = {
-			"/clientes",
-			"/auth/forgot/**"
-	};
-	
+	private Environment env;
+
+	private static final String[] PUBLIC_MACHERS = { "/h2-console/**" };
+
+	private static final String[] PUBLIC_MACHERS_GET = { "/produtos/**", "/categorias/**", "/estados/**" };
+
+	private static final String[] PUBLIC_MACHERS_POST = { "/clientes", "/auth/forgot/**" };
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
+
 		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
-            http.headers().frameOptions().disable();
-        }
-		
+			http.headers().frameOptions().disable();
+		}
+
 		http.cors().and().csrf().disable();
-		http.authorizeRequests()
-			.antMatchers(PUBLIC_MACHERS).permitAll()
-			.antMatchers(HttpMethod.GET, PUBLIC_MACHERS_GET).permitAll()
-			.antMatchers(HttpMethod.POST, PUBLIC_MACHERS_POST).permitAll()
-			.anyRequest().authenticated();
+		http.authorizeRequests().antMatchers(PUBLIC_MACHERS).permitAll().antMatchers(HttpMethod.GET, PUBLIC_MACHERS_GET)
+				.permitAll().antMatchers(HttpMethod.POST, PUBLIC_MACHERS_POST).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
-	
+
 	@Override
-	public void configure(AuthenticationManagerBuilder auth) throws Exception{
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 	}
-	
+
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
@@ -83,10 +72,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
 	}
-	
+
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**", "/configuration/**",
+				"/swagger-ui.html", "/webjars/**");
 	}
 
 }
